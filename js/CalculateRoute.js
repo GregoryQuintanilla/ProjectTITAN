ce552047a86129f37f6aa4dbf78654de01787d71
Gregory Quintanilla <gquintanilla1@pride.hofstra.edu>
2020-04-07 20:35:08 -0400

    updated code with route display functionality

--- js/CalculateRoute.js
+++ js/CalculateRoute.js
@@ -1,32 +1,31 @@
-var route = []
+var route = [] //array to house the completed route
 
-function calcRoute(origin, stops){
+function calcRoute(origin, stops, directionsService, directionsRenderer){
+     //function to start the route calculation kick off. uses inputted origina on desired stops and the disatance matrix service to create the route.
      var distanceMatrix = new google.maps.DistanceMatrixService();
-     route.push(origin[0].toString());
+     route.push(origin[0].toString()); // put the desired origin as teh first element of the list.
 
      distanceMatrix.getDistanceMatrix(
      {
                origins: origin,
                destinations: stops,
                travelMode: 'DRIVING',
-     },chooseNextDestination);
-     return route
+     },function(response,status){chooseNextDestination(response, status, directionsService, directionsRenderer )}); // create the distance matrix and then call the chooseNextDestination callback function
+     return route //eventually return the completed route
 }
 
-function chooseNextDestination(response, status){
-     if (status =='OK'){
-          var distanceMatrix = new google.maps.DistanceMatrixService()
-          var origin = response.originAddresses;
-          var destinations = response.destinationAddresses;
-          var shortestDest =  null;
-          var shortestDist = Infinity;
-          var shortestTime = Infinity;
-          var index = -1;
-          var SortBy = 'DISTANCE'
-          var nextStops = response.rows[0].elements;
-
-          //console.log(origin);
-          //console.log(destinations);
+function chooseNextDestination(response, status, directionsService, directionsRenderer){
+     if (status =='OK'){ //if the callback was successful
+          // set up initial variables
+          var distanceMatrix = new google.maps.DistanceMatrixService() // distance matrix function so that we can eventually recursively call
+          var origin = response.originAddresses; // origin point of this step of the route finding process. the 'last' chosen address.
+          var destinations = response.destinationAddresses; // the rest of the address still to be processed
+          var shortestDest =  null; // initialize the shortest destinatio with a sentinel
+          var shortestDist = Infinity;// initialize the shortest distance with a sentinel
+          var shortestTime = Infinity;// initialize the shortest time with a sentinel
+          var index = -1; //initialize the index of the shortest destination
+          var SortBy = 'DISTANCE' // keyword for how we want the route calculated. by DISTANCE or by TIME
+          var nextStops = response.rows[0].elements; //the distances from the last chosen address to the remaining addresses
 
           for (let i = 0; i < nextStops.length; i++){
                let element = nextStops[i]
@@ -59,15 +58,42 @@ function chooseNextDestination(response, status){
                     origins: [newOrigin],
                     destinations: destinations,
                     travelMode: 'DRIVING',
-               },chooseNextDestination);
+               },function(response, status) {chooseNextDestination(response, status, directionsService, directionsRenderer )}} );
           }
           //console.log(destinations);
 
 
      }
-     printRoute();
+     displayRoute(route,directionsService,directionsRenderer)
 }
 
+function displayRoute(routeToDisplay,directionsService,directionsRenderer){
+     let routeLength = routeToDisplay.length;
+     var routePackage = {'origin': routeToDisplay[0],'stops':[],'destination':routeToDisplay[routeLength-1]};
+
+     for(let i = 1; i < routeLength-1;i++){
+          routePackage['stops'].push({location:routeToDisplay[i],stopover:true});
+     }
+     directionsService.route({
+         origin: routePackage['origin'],
+         destination: routePackage['destination'],
+         waypoints: routePackage['stops'],
+         travelMode: 'DRIVING',
+
+         },
+         function(response, status) {
+              if (status === 'OK') {
+                   directionsRenderer.setDirections(response);
+              }
+              else {
+                   window.alert('Directions request failed due to ' + status);
+              }
+         });
+}
+
+function displayRoute(){
+
+}
 function printRoute(){
      console.log(route)
 }
