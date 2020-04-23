15676e4331b3c5cef365f79d975db5316946616c
Gregory Quintanilla <gquintanilla1@pride.hofstra.edu>
2020-04-22 21:15:54 -0400

    Trying to fix merges

--- FrontEnd/src/components/pages/AdminPage.js
+++ FrontEnd/src/components/pages/AdminPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import {MDBTabPane, MDBTabContent,MDBNavbar, MDBNavbarBrand, MDBNavbarNav,MDBNavItem, MDBContainer, MDBCard, MDBCol, MDBRow, MDBCardText, MDBBtn, MDBIcon} from 'mdbreact';
+import {MDBTabPane, MDBTabContent,MDBNavbar, MDBNavbarBrand, MDBNavbarNav,MDBNavItem, MDBContainer, MDBCard, MDBCol, MDBRow, MDBCardText, MDBBtn, MDBIcon, MDBCardBody, MDBInput} from 'mdbreact';
 import  {GoogleMap, LoadScript,DirectionsService, DirectionsRenderer, DistanceMatrixService} from '@react-google-maps/api';
 import "./AdminPage.css";
 import * as routeBuilder from './routeBuilder.js';
@@ -38,11 +38,11 @@ class AdminPage extends Component {
                 <MDBCol  className={"uisection"} sm="3">
                     <MDBRow className={"row1"}>
                         <MDBCol size="12" className={"naviCol"}>
-                            <MDBNavbar  className={"NavigationBar"} color="default-color" dark expand="md">
+                            <MDBNavbar  className={"NavigationBar"}  dark expand="md">
                                 <MDBNavbarBrand left className={"TITAN"}>
                                     <strong className="white-text">TITAN</strong>
                                 </MDBNavbarBrand>
-                                <MDBNavbarNav center>
+                                <MDBNavbarNav left>
                                     <MDBNavItem>
                                         <Link
                                             link
@@ -51,7 +51,7 @@ class AdminPage extends Component {
                                             onClick={this.toggle("1")}
                                             role="tab"
                                         >
-                                            <MDBBtn>Drivers</MDBBtn>
+                                            <MDBBtn >Drivers</MDBBtn>
                                         </Link>
                                     </MDBNavItem>
                                     <MDBNavItem>
@@ -62,7 +62,13 @@ class AdminPage extends Component {
                                             onClick={this.toggle("2")}
                                             role="tab"
                                         >
-                                            <MDBBtn>Stops</MDBBtn>
+                                            <MDBBtn >Stops</MDBBtn>
+                                        </Link>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <Link link to={"/"}>
+                                            <MDBBtn className={"LogoutBtn"}>
+                                                <MDBIcon icon={"power-off"}></MDBIcon>Logout</MDBBtn>
                                         </Link>
                                     </MDBNavItem>
 
@@ -79,20 +85,25 @@ class AdminPage extends Component {
                             >
                                 <MDBTabPane tabId="1" role="tabpanel" className={"tabpane"}>
                                     <MDBCard className={"DriversCard"}>
-                                        <MDBCard className={"test"}>
+                                        <Link
+                                            link
+                                            to="#"
+                                            active={this.state.activeItem === "5"}
+                                            onClick={this.toggle("5")}
+                                            role="tab">
+                                            <MDBBtn className={"DriverBtn"} color={"primary"}>Add Drivers</MDBBtn>
+                                        </Link>
+                                        <MDBCard className={"MiddleCard"}>
                                             <MDBIcon size ="2x" className={"icon"} icon={"car-alt"}></MDBIcon>
                                             <MDBCardText className={"text"}>
                                                 No Drivers
                                             </MDBCardText>
                                         </MDBCard>
-                                        <MDBBtn className={"AddBtn"}>
-                                           <MDBIcon size ="2x" icon={"plus"}></MDBIcon>
-                                        </MDBBtn>
                                     </MDBCard>
                                 </MDBTabPane>
                                 <MDBTabPane tabId="2" role="tabpanel" className={"tabpane"}>
                                     <MDBCard  className={"StopsCard "} >
-                                        <MDBCard className={"test"}>
+                                        <MDBCard className={"MiddleCard"}>
                                             <MDBIcon size ="2x" className={"icon"} icon={"map-pin"}></MDBIcon>
                                             <MDBCardText className={"text"}>
                                                 No Routes
@@ -118,11 +129,170 @@ class AdminPage extends Component {
                                         </Link>
 
 
-                                        <MDBBtn className= {"CalculateBtn"} onClick={(e) => {routeBuilder.calcRoute(this.state.origin, this.state.stops, this.state.directionsService, this.state.directionsRenderer, this.state.distanceMatrixService)}}>
+                                        <MDBBtn className= {"CalculateBtn"} onClick={() => {routeBuilder.calcRoute(this.state.origin, this.state.stops, this.state.directionsService, this.state.directionsRenderer, this.state.distanceMatrixService)}}>
                                             Calculate
                                         </MDBBtn>
 
 
+
+                                    </MDBCard>
+                                </MDBTabPane>
+                                <MDBTabPane tabId="5" role="tabpanel" className={"tabpane"}>
+                                    <MDBCard className={"AddDriversCard"}>
+                                        <MDBCard className={"FormCard"}>
+                                            <MDBCardBody>
+                                                <form >
+                                                    <p className="h4 text-center py-4">Input A Driver</p>
+                                                    <div className="grey-text">
+                                                        <MDBInput
+                                                            label="Name"
+                                                            icon="user"
+                                                            group
+                                                            type="text"
+                                                            validate
+                                                            error="wrong"
+                                                            success="right"
+                                                            type="text"
+                                                            name="username"
+                                                            value={this.state.username}
+                                                            onChange={this.handleChange}
+                                                        />
+                                                        <MDBInput
+                                                            label="ID Number"
+                                                            icon="id-card"
+                                                            group
+                                                            type="text"
+                                                            validate
+                                                            error="wrong"
+                                                            success="right"
+                                                        />
+                                                    </div>
+
+                                                    <MDBBtn color="red" className={"btn-block3"} >
+                                                        Remove Last
+                                                    </MDBBtn>
+                                                    <MDBBtn color="primary" className={"btn-block4"} type="click" >
+                                                        Add
+                                                    </MDBBtn>
+
+                                                </form>
+                                            </MDBCardBody>
+                                        </MDBCard>
+                                        <MDBBtn className={"SaveChanges"} color={"primary"}>
+                                            Save Changes
+                                        </MDBBtn>
+                                        <Link link
+                                              to="#"
+                                              active={this.state.activeItem === "1"}
+                                              onClick={this.toggle("1")}
+                                              role="tab">
+                                            <MDBBtn className={"Back"} color={"red"}>
+                                                Back
+                                            </MDBBtn>
+                                        </Link>
+                                    </MDBCard>
+
+                                </MDBTabPane>
+                                <MDBTabPane tabId="4" role="tabpanel" className={"tabpane"}>
+                                    <MDBCard className={"UploadStopsCard"}>
+                                        <MDBCard className={"FormCard"}>
+                                        <MDBCardBody>
+                                            <form className={"Form"}>
+                                                <p className="h4 text-left py-4">Upload A File</p>
+                                                <div className="grey-text">
+                                                    <input type="file" onChange={this.onChange} />
+                                                    <button type="submit">Upload</button>
+                                                </div>
+                                            </form>
+
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                        <MDBBtn className={"SaveChanges"} color={"primary"}>
+                                            Save Changes
+                                        </MDBBtn>
+                                        <Link link
+                                              to="#"
+                                              active={this.state.activeItem === "1"}
+                                              onClick={this.toggle("2")}
+                                              role="tab">
+                                            <MDBBtn className={"Back"} color={"red"}>
+                                                Back
+                                            </MDBBtn>
+                                        </Link>
+
+                                    </MDBCard>
+
+                                </MDBTabPane>
+                                <MDBTabPane tabId="3" role="tabpanel" className={"tabpane"}>
+                                    <MDBCard className={"AddStopsCard"}>
+                                            <MDBCard className={"FormCard"}>
+                                                <MDBCardBody>
+                                                    <form >
+                                                        <p className="h4 text-center py-4">Input an Address</p>
+                                                        <div className="grey-text">
+                                                            <MDBInput
+                                                                label="Street Address"
+                                                                icon="road"
+                                                                group
+                                                                type="text"
+                                                                validate
+                                                                error="wrong"
+                                                                success="right"
+                                                                type="text"
+                                                                name="username"
+                                                                value={this.state.username}
+                                                                onChange={this.handleChange}
+                                                            />
+                                                            <MDBInput
+                                                                label="City"
+                                                                icon="city"
+                                                                group
+                                                                type="text"
+                                                                validate
+                                                                error="wrong"
+                                                                success="right"
+                                                            />
+                                                            <MDBInput
+                                                                label="State"
+                                                                icon="flag-usa"
+                                                                group
+                                                                type="text"
+                                                                validate
+                                                                error="wrong"
+                                                                success="right"
+                                                            />
+                                                            <MDBInput
+                                                                label="Zip Code"
+                                                                icon="map-marker-alt"
+                                                                group
+                                                                type="text"
+                                                                validate
+                                                            />
+                                                        </div>
+
+                                                            <MDBBtn color="red" className={"btn-block2"} >
+                                                                Remove Last
+                                                            </MDBBtn>
+                                                            <MDBBtn className={"btn-block1"} type="click" >
+                                                                Add
+                                                            </MDBBtn>
+
+                                                    </form>
+                                                </MDBCardBody>
+                                            </MDBCard>
+
+                                        <MDBBtn className={"SaveChanges"} color={"primary"}>
+                                            Save Changes
+                                        </MDBBtn>
+                                        <Link link
+                                              to="#"
+                                              active={this.state.activeItem === "1"}
+                                              onClick={this.toggle("2")}
+                                              role="tab">
+                                            <MDBBtn className={"Back"} color={"red"}>
+                                                Back
+                                            </MDBBtn>
+                                        </Link>
 
                                     </MDBCard>
                                 </MDBTabPane>
